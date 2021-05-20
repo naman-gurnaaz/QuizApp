c2c0dcb02333bc6577af0996909482c5a7b55557
Namanpreet Kaur <69746852+naman-gurnaaz@users.noreply.github.com>
2021-05-21 01:30:20 +0530

    Basic Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,37 @@
-void main() {
-  
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favourite animal?"
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
