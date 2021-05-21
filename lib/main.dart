6b7d17f12e00b8b7520fe055eaeb13682e71697b
Namanpreet Kaur <69746852+naman-gurnaaz@users.noreply.github.com>
2021-05-21 21:39:49 +0530

    Using lists and mapping, splitting widgets and using callback funcs

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'package:first_app/result.dart';
 import 'package:flutter/material.dart';
-import './question.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
@@ -12,10 +13,50 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answer': [
+        'Red',
+        'Black',
+        'Lavender',
+        'Emerald',
+      ]
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answer': [
+        'Dogs',
+        'Cats',
+        'Turtle',
+        'Giraffe',
+      ]
+    },
+    {
+      'questionText': 'Who\'s your favourite Harry Potter Character?',
+      'answer': [
+        'Luna Lovegood',
+        'Bellatrix Lestrange',
+        'Prof. Albus Dumbledore',
+        'Prof. Minerva McGonnagal',
+      ]
+    },
+    {
+      'questionText': 'What\'s your Harry Potter house?',
+      'answer': [
+        'Gryffindor',
+        'Hufflepuff',
+        'Ravenclaw',
+        'Slytherin',
+      ]
+    }
+  ];
 
   var _questionIndex = 0;
 
   void _answerQuestion() {
+    if (_questionIndex < _questions.length) {}
+
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -24,36 +65,18 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-
-    var questions = [
-      "What's your favorite color?",
-      "What's your favourite animal?"
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My First App'),
+          title: Text('Quiz that doesn\'t really tell you anything.'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex],
-            ),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: _answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: _answerQuestion,
-            ),
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+              answerQuestion :_answerQuestion,
+               questions: _questions,
+               questionIndex: _questionIndex,
+            )
+            : Result(),
       ),
     );
   }
